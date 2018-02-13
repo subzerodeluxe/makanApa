19fec580f4c323f7a0d0edec8d73f522eb2382fd
subzerodeluxe <maarten.waanders@gmail.com>
2018-02-13 12:14:11 +0100

    Animated the logo

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @IonicPage({
   name: 'welcome'
@@ -8,17 +9,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-welcome',
   templateUrl: 'welcome.html',
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave',[ // "* => void"
+        animate('0.6s ease-out', keyframes([
+          style({ opacity: 1, transform: 'translate3d(0, 0 ,0)', offset: 0}),
+          style({ opacity: 0.5, transform: 'translate3d(-20%, 0 ,0)', offset: 0.7}),
+          style({ opacity: 0, transform: 'translate3d(100%, 0 ,0)', offset: 1})
+          ]))
+        ])
+      ])
+    ]
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    setTimeout(() => {
-      this.skipPage()
-    }, 5000)
-  }
+  show: boolean = true;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad WelcomePage');
+    
+    setTimeout(() => {
+      this.startAnimate();
+      //this.skipPage();
+    }, 3000); 
+  }
+
+  startAnimate() {
+    this.show = !this.show;
+    setTimeout(() => {
+      this.skipPage();
+    }, 800);
   }
 
   skipPage() {
