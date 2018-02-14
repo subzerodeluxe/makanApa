59423cbe2ba7003ced35709bdb3f1d3ec5498fa0
subzerodeluxe <maarten.waanders@gmail.com>
2018-02-14 20:39:42 +0100

    Bug fix in list

--- src/components/dishes-modal/dishes-modal.ts
+++ src/components/dishes-modal/dishes-modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, ViewController } from 'ionic-angular';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Platform } from 'ionic-angular/platform/platform';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
@@ -16,11 +16,11 @@ export class DishesModalComponent {
   dishes: any;
   showInput: boolean = false;
   showDish: boolean = true;
-  showCheckmark: boolean = true; 
+  showCheckmark: boolean = false; 
   timeout = null;
   enteredDish: string; 
 
-  constructor(public platform: Platform, public screenOrientation: ScreenOrientation) {
+  constructor(public platform: Platform, public screenOrientation: ScreenOrientation, public viewCtrl: ViewController) {
     if(this.platform.is('cordova')) {
       // set to landscape
      this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
@@ -28,7 +28,7 @@ export class DishesModalComponent {
   }
 
   ionViewDidLoad() {
-    this.dishes = ["Sop", "Nasi Goreng", "Pancakes", "Stamppot Andijvie", "Preitaart"]; 
+    this.dishes = ["Sop", "Nasi Goreng", "Pancakes", "Stamppot Andijvie", "...", "..."]; 
   }
   
   addDish(dish) {
@@ -45,6 +45,20 @@ export class DishesModalComponent {
     if(index > -1) {
       this.dishes[index] = this.enteredDish; 
     }
+  }
+
+
+  checkInput(input) {
+    console.log("De tekst: " + input.value);
+
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      if(input.value >= 1) {
+        this.showCheckmark = true;
+      } else {
+        this.showCheckmark = false; 
+      }
+    }, 500);
   }
 
   deleteDish(dish){
@@ -64,8 +78,10 @@ export class DishesModalComponent {
     console.log("Clicked on dish: " + JSON.stringify(dishIndex));
   }
 
-  showingInput(input) {
-    console.log("De tekst: " + JSON.stringify(input));
+  closeMenuModal() {
+    console.log("The modal is closed!"); 
+    this.viewCtrl.dismiss();
   }
+ 
 
 } 
